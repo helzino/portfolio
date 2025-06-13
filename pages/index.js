bebf3b2a8e14beb8118a28b1422cde58bcaad23e
helzino <helenawreford@live.com>
2025-06-13 17:45:14 +0100

    Add Google site verification meta tag

--- pages/index.js
+++ pages/index.js
@@ -52,27 +52,31 @@ export default function Home() {
     <div className={`relative ${data.showCursor && "cursor-none"}`}>
       {data.showCursor && <Cursor />}
       <Head>
-        <title>{data.name} - Zoology Student & Filmmaker</title>
-        <meta
-          name="description"
-          content="I'm a Zoology Student and filmmaker with a passion for bringing the natural world to life through storytelling. From capturing breathtaking drone footage to diving into marine research, my work is all about inspiring others to connect with our planet. I thrive on new challenges, whether it's trekking with camera gear to remote locations or translating complex science into captivating stories. My mission? To share the wonder of wildlife and spark a sense of adventure and conservation in every audience I reach."
-        />
-        <meta property="og:title" content={`${data.name} - Zoology Student & Filmmaker`} />
-        <meta
-          property="og:description"
-          content="I'm a Zoology Student and filmmaker with a passion for bringing the natural world to life through storytelling. From capturing breathtaking drone footage to diving into marine research, my work is all about inspiring others to connect with our planet. I thrive on new challenges, whether it's trekking with camera gear to remote locations or translating complex science into captivating stories. My mission? To share the wonder of wildlife and spark a sense of adventure and conservation in every audience I reach."
-        />
-        <meta property="og:image" content="/favicon.ico" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={`${data.name} - Zoology Student & Filmmaker`} />
-        <meta
-          name="twitter:description"
-          content="I'm a Zoology Student and filmmaker with a passion for bringing the natural world to life through storytelling. From capturing breathtaking drone footage to diving into marine research, my work is all about inspiring others to connect with our planet. I thrive on new challenges, whether it's trekking with camera gear to remote locations or translating complex science into captivating stories. My mission? To share the wonder of wildlife and spark a sense of adventure and conservation in every audience I reach."
-        />
-        <meta name="twitter:image" content="/favicon.ico" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  <title>{data.name} - Zoologist & Filmmaker</title>
+  <meta
+    name="description"
+    content="I'm a Zoology Student and filmmaker with a passion for bringing the natural world to life through storytelling. From capturing breathtaking drone footage to diving into marine research, my work is all about inspiring others to connect with our planet. I thrive on new challenges, whether it's trekking with camera gear to remote locations or translating complex science into captivating stories. My mission? To share the wonder of wildlife and spark a sense of adventure and conservation in every audience I reach."
+  />
+  <meta property="og:title" content={`${data.name} - Zoology Student & Filmmaker`} />
+  <meta
+    property="og:description"
+    content="I'm a Zoology Student and filmmaker with a passion for bringing the natural world to life through storytelling. From capturing breathtaking drone footage to diving into marine research, my work is all about inspiring others to connect with our planet. I thrive on new challenges, whether it's trekking with camera gear to remote locations or translating complex science into captivating stories. My mission? To share the wonder of wildlife and spark a sense of adventure and conservation in every audience I reach."
+  />
+  <meta property="og:image" content="/favicon.ico" />
+  <meta property="og:type" content="website" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content={`${data.name} - Zoology Student & Filmmaker`} />
+  <meta
+    name="twitter:description"
+    content="I'm a Zoology Student and filmmaker with a passion for bringing the natural world to life through storytelling. From capturing breathtaking drone footage to diving into marine research, my work is all about inspiring others to connect with our planet. I thrive on new challenges, whether it's trekking with camera gear to remote locations or translating complex science into captivating stories. My mission? To share the wonder of wildlife and spark a sense of adventure and conservation in every audience I reach."
+  />
+  <meta name="twitter:image" content="/favicon.ico" />
+  
+  {/* Google Site Verification Meta Tag */}
+  <meta name="google-site-verification" content="x_VXLipNZRtrP88jfkLbC2kdGGRxIBAgR4Achp5XKvU" />
+
+  <link rel="icon" href="/favicon.ico" />
+</Head>
 
       <div className="gradient-circle"></div>
       <div className="gradient-circle-bottom"></div>
